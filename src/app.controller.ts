bdcffa54e01ad2e8877f87e893fb799759a4f0f3
davamo <davamo@davamo.cl>
2022-08-06 22:44:59 -0400

    feature:  endPoint get new

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('new')
+  getNewEndpoint(): string {
+    return this.appService.getEndpoint();
+  }
 }
